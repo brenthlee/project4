a14259e93b4fda68ab452476bed13872aeca0501
Brent Lee <brenthlee@gmail.com>
2018-11-05 09:14:56 -0800

    initial commit

---
+++ ChangeMaker.java
@@ -0,0 +1,13 @@
+/*
+ * Brent Lee (blee96)
+ * Kaitlin Bleich (kbleich)
+ * 11/05/2018
+ * Project 4
+ */
+
+public class ChangeMaker {
+
+    public static void main(String[] args) {
+        
+    }
+}
